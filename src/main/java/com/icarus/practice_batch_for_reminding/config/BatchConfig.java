871cc83644698f91add10a0f5689b6f08a1a3075
icarus8050 <icarus8050@naver.com>
2021-06-01 00:25:59 +0900

    properties init

---
+++ src/main/java/com/icarus/practice_batch_for_reminding/config/BatchConfig.java
@@ -0,0 +1,9 @@
+package com.icarus.practice_batch_for_reminding.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+}
