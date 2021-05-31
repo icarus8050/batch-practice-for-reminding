a8b588884b79b5a4567b62dda831019e9d13021d
icarus8050 <icarus8050@naver.com>
2021-05-31 19:39:32 +0900

    Initial commit

---
+++ src/main/java/com/icarus/practice_batch_for_reminding/BatchPracticeForRemindingApplication.java
@@ -0,0 +1,13 @@
+package com.icarus.practice_batch_for_reminding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPracticeForRemindingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchPracticeForRemindingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icarus/practice_batch_for_reminding/BatchPracticeForRemindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icarus.practice_batch_for_reminding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPracticeForRemindingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
